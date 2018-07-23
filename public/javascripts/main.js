dee93db5a39900cf1bdeef63b01aa4249ed06d71
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-07-23 13:01:51 +0530

    Initial Prototype'

---
+++ public/javascripts/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+$('#myCarousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$(".schedule-tab").click(function(e){
+    e.preventDefault();
+    $('#myCarousel').trigger("to.owl.carousel", [1, 750, true]);
+});
+$(".talks-tab").click(function(e){
+    e.preventDefault();
+    $('#myCarousel').trigger("to.owl.carousel", [2, 750, true]);
+});
+$(".workshops-tab").click(function(e){
+    e.preventDefault();
+    $('#myCarousel').trigger("to.owl.carousel", [3, 750, true]);
+});
+$(".code-of-conduct-tab").click(function(e){
+    e.preventDefault();
+    $('#myCarousel').trigger("to.owl.carousel", [4, 750, true]);
+});
+$(".about-tab").click(function(e){
+    e.preventDefault();
+    $('#myCarousel').trigger("to.owl.carousel", [5, 750, true]);
+});
+
+});
